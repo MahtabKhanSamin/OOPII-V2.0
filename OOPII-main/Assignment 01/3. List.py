4622ecad7360756ad8e317103e1dfe59e2db478e
Mahtab Khan <khan22205101608@diu.edu.bd>
2024-09-16 15:58:30 +0600

    Update 3. List.py

---
+++ OOPII-main/Assignment 01/3. List.py
@@ -0,0 +1,10 @@
+customers=["Alice", "Bob", "Charlie", "David", "Eve"]
+print(customers[2])
+customers[1]='Ben'
+print(customers)
+customers.append('Frank')
+print(customers)
+customers.remove('David')
+print(customers)
+customers.sort()
+print(customers)
