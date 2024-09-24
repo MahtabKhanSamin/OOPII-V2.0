33dc69fcf4488698a7c7fa187b10ef877d29ecf5
MahtabKhanSamin <165150635+MahtabKhanSamin@users.noreply.github.com>
2024-09-24 10:59:20 +0600

    Update 28. Python Program to Display Fibonacci Sequence Using Recursion.py

---
+++ OOPII-main/Lab Report 01/28. Python Program to Display Fibonacci Sequence Using Recursion.py
@@ -0,0 +1,11 @@
+def fib(b,c,d):
+    e=b+c
+    if d==a:
+        return
+    else:
+        print(e,end=" ")
+        d+=1
+        fib(c,e,d)
+a=int(input("How many numbers do you want: "))
+print(0,1,end=" ")
+fib(0,1,2)
