96725f7d654baaefc1b430272e2f27fedcefde48
Mahtab Khan <khan22205101608@diu.edu.bd>
2024-11-21 15:43:12 +0600

    Update 245.py

--- Lab Task 10/21/11/245.py
+++ Lab Task 10/21/11/245.py
@@ -1,10 +1,11 @@
-a=[10,20,30,0]
 try:
+    a = [10, 20, 30, 0]
     b=int(input("Enter the Dividend: "))
-    for x in a:
-        print(x/b)
-except ZeroDivisionError:
-    print("Don't Divide by Zero you Idiot")
+    try:
+        for x in a:
+            print(x/b)
+    except ZeroDivisionError:
+        print("Don't Divide by Zero you Idiot")
 except ValueError:
     print("Input a correct value you noob")
 except NameError:
